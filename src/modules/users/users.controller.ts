439d6e1ed87543a712b07b040d61bd09db6ef10a
tuananhkim555 <76089113+tuananhkim555@users.noreply.github.com>
2025-01-18 22:07:44 +0700

    Update users.controller.ts

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -39,7 +39,7 @@ interface RequestWithUser extends ExpressRequest {
 }
 
 @Controller('/api/users')
-@UseGuards(JwtAuthGuard)
+//@UseGuards(JwtAuthGuard)
 @ApiBearerAuth('JWT-auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
